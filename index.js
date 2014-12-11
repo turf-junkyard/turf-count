f18068e9c27145a940a27e4ee86dcc0888c995e5
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 11:35:55 -0500

    stylez

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var inside = require('turf-inside')
+var inside = require('turf-inside');
 
 module.exports = function(polyFC, ptFC, outField, done){
   polyFC.features.forEach(function(poly){
-    if(!poly.properties){
-      poly.properties = {}
-    }
-    var values = []
+    if(!poly.properties) poly.properties = {};
+    var values = [];
     ptFC.features.forEach(function(pt){
       if (inside(pt, poly)) {
-        values.push(1)
+        values.push(1);
       }
     })
-    poly.properties[outField] = values.length
+    poly.properties[outField] = values.length;
   })
 
   return polyFC;
