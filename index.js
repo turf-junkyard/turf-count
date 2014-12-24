d9f158fb0e02193343056ca72f4819b902e37f72
Tom MacWright <tom@macwright.org>
2014-12-23 22:47:56 -0500

    Optimize
    The test case only has two polygons, so it's not super representative -
    I'll add a better bench soon. This gives a >10x speedup for large sets
    of polygons, like our hexbin example. This depends on a new release for
    turf-extent to be fast.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var inside = require('turf-inside');
+var inside = require('turf-inside'),
+    extent = require('turf-extent');
 
 module.exports = function(polyFC, ptFC, outField, done){
   for (var i = 0; i < polyFC.features.length; i++) {
     var poly = polyFC.features[i];
     if(!poly.properties) poly.properties = {};
     var values = 0;
+    var bbox = extent(poly);
     for (var j = 0; j < ptFC.features.length; j++) {
       var pt = ptFC.features[j];
-      if (inside(pt, poly)) {
-        values++;
-      }
+      var coords = pt.geometry.coordinates;
+      if (coords[0] > bbox[0] &&
+        coords[1] > bbox[1] &&
+        coords[0] < bbox[2] &&
+        coords[1] < bbox[3] &&
+        inside(pt, poly)) values++;
     }
     poly.properties[outField] = values;
   }
 
   return polyFC;
 };
+
