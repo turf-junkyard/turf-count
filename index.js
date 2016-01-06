d89839c37f22e4e8ddf62056f8510eeb759aeb3c
Tim Channell <tim.channell@gmail.com>
2016-01-06 15:31:30 -0500

    bump to 1.0.3, add eslint

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ var inside = require('turf-inside');
  * //=result
  */
 
-module.exports = function(polyFC, ptFC, outField) {
+module.exports = function (polyFC, ptFC, outField) {
   for (var i = 0; i < polyFC.features.length; i++) {
     var poly = polyFC.features[i];
-    if(!poly.properties) poly.properties = {};
+    if (!poly.properties) poly.properties = {};
     var values = 0;
     for (var j = 0; j < ptFC.features.length; j++) {
       var pt = ptFC.features[j];
